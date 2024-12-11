dd280867e3aee1aa20c5e5e968a6f839c738999b
tech-balaji-chennai <tech.balajichennai@gmail.com>
2024-12-11 18:06:29 +0530

    Problem 3

---
+++ 1) Number Problems/3) Odd Or Even Number/Odd Or Even Number - Python Program.py
@@ -0,0 +1,15 @@
+#Odd Or Even Number - Python Program
+
+num = int(input())
+
+#Logic 1
+if (num % 2 == 0)):
+    print("Even")
+else:
+    print("Odd")
+
+#Logic 2
+if (num % 2 == 1):
+    print("Odd")
+else:
+    print("Even")
