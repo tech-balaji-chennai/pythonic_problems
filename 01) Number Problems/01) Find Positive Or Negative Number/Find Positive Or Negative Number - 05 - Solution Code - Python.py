fbab88b72d6eba0da673ed734a6e00624a255203
tech-balaji-chennai <tech.balajichennai@gmail.com>
2025-06-12 20:21:58 +0530

    update problems

--- 01) Number Problems/01) Find Positive Or Negative Number/Find Positive Or Negative Number - 05 - Solution Code - Python.py
+++ 01) Number Problems/01) Find Positive Or Negative Number/Find Positive Or Negative Number - 05 - Solution Code - Python.py
@@ -1,10 +1,14 @@
 #Find Positive Or Negative Number - 05 - Solution Code - Python.py
 
-num = int(input())
+try:
+    num = int(input())
 
-if (num > 0):
-    print("Positive")
-elif (num < 0):
-    print("Negative")
-else:
-    print("Neither Positive Nor Negative")
+    if (num > 0):
+        print("Positive")
+    elif (num < 0):
+        print("Negative")
+    else:
+        print("Neither Positive Nor Negative")
+
+except ValueError as e:
+    print(f"Invalid input. Please enter an integer value")
--- 01) Number Problems/02) Absolute Value Of A Number/Absolute Value Of A Number - 05 - Solution Code - Python.py
+++ 01) Number Problems/02) Absolute Value Of A Number/Absolute Value Of A Number - 05 - Solution Code - Python.py
@@ -1,8 +1,12 @@
 #Absolute Value Of A Number - 05 - Solution Code - Python
 
-num = int(input())
+try:
+    num = int(input())
 
-if (num < 0):
-    num = -num
+    if (num < 0):
+        num = -num
 
-print(num)
+    print(num)
+
+except ValueError as e:
+    print(f"Invalid input. Please enter an integer value")
